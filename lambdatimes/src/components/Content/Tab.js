df4e4f292ec4a4f777832d734f3655efac0e7d48
harjothkhara <harjoth.khara@gmail.com>
2019-05-03 10:50:33 -0700

    stylized components for tab--tricky, and tabs

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,20 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types'
+import styled from 'styled-components';
+
+
+const TabDiv = styled.div `
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  margin: 0 5px;
+  padding: 2px 10px;
+  font-size: 12px;
+  letter-spacing: 2px;
+  cursor: pointer;
+  font-weight: bold;
+  &:hover {
+    text-decoration: underline;
+  }
+`
+
+const dynamicStyle = {
+  backgroundColor: '#fff',
+  color: '#333',
+  border: '2px solid #333'
+}
+
+
 
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
   return (
-    <div
-      className={`tab ${props.tab === props.selectedTab ? 'active-tab' : ''}`}
-      onClick={() => { props.selectTabHandler(props.tab)
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
-      }}
-    >
+    <TabDiv onClick={() => props.selectTabHandler(props.tab)}
+        style = {props.tab === props.selectedTab ? dynamicStyle : {}}>
+      
+        {/* Replace this dummy click handler function with your selectTabHandler function from props 
+         you'll need to pass the `tab` in as an argument to this handler.  */}
+     
       {props.tab.toUpperCase()}
-    </div>
+    </TabDiv>
   );
 };
 
--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,19 +1,47 @@
 import React from 'react';
 import Tab from './Tab';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
+const TabsDiv = styled.div `
+  border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  width: 100%;
+  height: 47px;
+  background-color: #fff;
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+
+`
+const Topics = styled.div `
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+
+`
+
+const TitleSpan = styled.span `
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+`
 
 const Tabs = props => {
   return (
-    <div className="tabs">
-      <div className="topics">
-        <span className="title">TRENDING TOPICS:</span>
+    <TabsDiv>
+      <Topics>
+        <TitleSpan>TRENDING TOPICS:</TitleSpan>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
           {props.tabs.map ( item => <Tab tab={item} key={item} selectTabHandler={props.selectTabHandler}
           selectedTab={props.selectedTab} /> )}
-      </div>
-    </div>
+      </Topics>
+    </TabsDiv>
   );
 };
 
