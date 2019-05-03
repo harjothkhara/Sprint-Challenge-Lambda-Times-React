f530c42f45172a777054c6a2923bed97030c80a3
harjothkhara <harjoth.khara@gmail.com>
2019-05-03 09:22:33 -0700

    new tab, card components with map, props passed to each comp, CDM to setState of tabs and cards to their dataset in Content, tab selector between different cards is functional

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -7,6 +7,8 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+          {props.tabs.map ( item => <Tab tab={item} key={item} selectTabHandler={props.selectTabHandler}
+          selectedTab={props.selectedTab} /> )}
       </div>
     </div>
   );
